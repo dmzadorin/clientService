daa7be7cbe87cc4299d54721404ef3df2ba2dd33
Dmitry Zadorin <dmzadorin@gmail.com>
2018-03-02 22:19:50 +0300

    Added integration infra

--- src/test/groovy/ru/dmzadorin/clientservice/integration/ClientServiceIntegrationTest.groovy
+++ src/test/groovy/ru/dmzadorin/clientservice/integration/ClientServiceIntegrationTest.groovy
@@ -19,10 +19,33 @@ class ClientServiceIntegrationTest extends Specification {
         httpServer.createContext("/", new HttpHandler() {
             @Override
             void handle(HttpExchange httpExchange) throws IOException {
+                BufferedReader input = new BufferedReader(new InputStreamReader(httpExchange.getRequestBody()))
+                def inputString = ''
+                def sb = new StringBuilder()
+                while ((inputString = input.readLine()) != null) {
+                    sb.append(inputString)
+                }
+                println("Input request: $sb")
+                def builder = new StringBuilder()
+                builder.append("RESPONSE!")
+                byte[] bytes = builder.toString().getBytes();
+                httpExchange.sendResponseHeaders(200, bytes.length);
 
+                OutputStream os = httpExchange.getResponseBody();
+                os.write(bytes);
+                os.close();
             }
         })
+        httpServer.start()
         url = new URL("http://localhost:8080/")
+    }
+
+    def "Simple request response test"() {
+        when:
+        def response = sendRequest("request")
+        then:
+        response != null
+        noExceptionThrown()
     }
 
     void cleanupSpec() {
@@ -31,12 +54,17 @@ class ClientServiceIntegrationTest extends Specification {
 
     String sendRequest(String request) {
         def sb = new StringBuilder()
+        HttpURLConnection connection = null
         try {
-            URLConnection connection = url.openConnection()
-            connection.setDoOutput(true)
+            connection = initConnection()
+
             OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream())
             out.write(request)
             out.close()
+
+            connection.connect()
+            int statusCode = connection.getResponseCode();
+
             BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()))
 
             def inputString = ''
@@ -44,9 +72,20 @@ class ClientServiceIntegrationTest extends Specification {
                 sb.append(inputString)
             }
             input.close();
-        } catch (IOException e){
+        } catch (IOException e) {
             throw new IllegalStateException(e)
+        } finally {
+            connection?.disconnect()
         }
         sb.toString()
+    }
+
+    def HttpURLConnection initConnection() throws IOException {
+        HttpURLConnection connection = ( HttpURLConnection ) url.openConnection();
+        connection.setRequestMethod( "POST" );
+        connection.setDoOutput( true );
+        connection.setDoInput( true );
+        connection.setInstanceFollowRedirects( false );
+        return connection;
     }
 }
